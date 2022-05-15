226323ecbcf51ce6ccbe494085e06ba620ac4185
jeff chen <chenjeff4840@vt.edu>
2022-05-15 16:20:04 -0400

    Uploaded 0.3, view README for details

--- KMPDownloader.py
+++ KMPDownloader.py
@@ -14,11 +14,15 @@ import cfscrape
 Simple kemono.party downloader relying on html parsing and download by url
 Using multithreading
 
-- Bulk download with file containing all artist main window links
+- Bulk download supporting post, single page, and all artist posts
+- Check when entering invalid urls
 - Robustness upgrades
 - Slight memory optimization
 - Fixed bug where file names were not being isolated properly
 - Switch to cfscrape library to mitigate cloudflare issues
+- Fixed issues where incomplete downloads would occur to large files
+- Multithreading can be done across multiple urls in bulk download file instead of one
+- Fixed issue where large files would not be downloaded entirely
 @author Jeff Chen
 @version 0.3
 @last modified 5/14/2022
@@ -26,15 +30,27 @@ Using multithreading
 
 # Settings ###################################################
 folder = r"D:/User Files/Personal/Cloud Drive/MEGAsync/Nonsensitive/Best/Package/Pixiv/~unsorted/"
-CHUNK_SIZE = 1024 * 1024 * 64 # Chunk size of downloads
-TIME_BETWEEN_CHUNKS = 2  # Required to not mimic a DDOS attack, DDOS guard may be triggered if you download too many files too quickly
-THREADS = 6                # Number of threads
+CHUNK_SIZE = 1024 * 1024 * 64 # Chunk size of downloads TODO implement in download_files
+TIME_BETWEEN_CHUNKS = 2  # Time between Internet activities
+THREADS = 6                # Number of threads, note that download size is bottleneck by numerous factors
+                           # More threads boost speed of downlaoding many small files but will not if
+                           # downloading a single large file caps download limit
+                           # Larger files may timeout, resulting in incomplete upgrades, if this occurs, decrease 
+                           # the number of threads or decrease the number of downloads
 # URL specific variables <MODIFY AT YOUR RISK> ###############
 containerPrefix = "https://kemono.party"
-##############################################################
+# DO NOT EDIT ################################################
 kill = False                     # Thread kill switch
 download_queue = queue.Queue(-1) # Download task queue
 downloadables = Semaphore(0)     # Avalible downloadable resource device
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class UnknownURLTypeException(Error):
+    """Raised when url type cannot be determined"""
+    pass
 
 class downThread(threading.Thread):
    """
@@ -57,12 +73,10 @@ class downThread(threading.Thread):
       """
       while True:
          # Wait until download is available
-         print(self.__name + " is ready")
          downloadables.acquire()
 
          # Check kill signal
          if kill:
-            print(self.__name + " has finished")
             return
          
          # Pop queue and download it
@@ -78,7 +92,8 @@ def trim_fname(fname:str) -> str:
    Trims fname, returns result. Extensions are kept:
    For example
    "/data/2f/33/2f33425e67b99de681eb7638ef2c7ca133d7377641cff1c14ba4c4f133b9f4d6.txt?f=File.txt"
-   -> 2f33425e67b99de681eb7638ef2c7ca133d7377641cff1c14ba4c4f133b9f4d6.txt \n
+   -> 2f33425e67b99de681eb7638ef2c7ca133d7377641cff1c14ba4c4f133b9f4d6.txt 
+
    Param: 
       fname: file name
    Pre: fname follows above convention
@@ -92,6 +107,7 @@ def trim_fname(fname:str) -> str:
 def download_file(src:str, fname:str, tname:str) -> None:
    """
    Downloads file at src. Skips duplicate files
+
    Param:
       src: src of image to download
       fname: what to name the file to download
@@ -100,19 +116,36 @@ def download_file(src:str, fname:str, tname:str) -> None:
 
    try:
       scraper = cfscrape.create_scraper()
-      data = scraper.get(src).content
+
+      # Get download size
+      r = scraper.request('HEAD', src)
+      fullsize = r.headers.get('Content-Length')
+
+      # Download file to memory
+      print(tname + " downloading " + src)
+      data = scraper.get(src)
+
+      # Loop until download size matches real size
+      while(data.headers.get('Content-Length') != fullsize):
+         print(tname + ": Fragmented downloaded -> Real: " + fullsize + " Actual: " + data.headers.get('Content-Length'))
+         print("Restarting download")
+         data = scraper.get(src)
+
+      # Download the file
+      with open(fname, 'wb') as fd:
+         fd.write(data.content)
+         fd.flush()
+      print(tname + " download complete")
+      scraper.close()
+
    except (URLError, HTTPError) as e:
-      print(tname + ": Download incomplete, trying again for " + src)
-      print(e.info)
+      print(tname + ": Download could not be completed for " + src)
       exit()
-   print(tname + " downloading " + src)
-   with open(fname, 'wb') as fd:
-      fd.write(data)
-   print(tname + " download complete")
 
 def download_all_files(imgLinks:ResultSet, dir:str)->None:
    """
    Puts all urls in imgLinks into download queue
+
    Param:
       imgLinks: all image links within a container
       dir: where to save the images
@@ -128,16 +161,17 @@ def download_all_files(imgLinks:ResultSet, dir:str)->None:
 def process_container(url:str, root:str)->None:
    """
    Processes a container which is the page used to store post content
+
    Supports
    - downloading all visable images
    - content divider (BUG other urls are included)
    - download divider
+
    Param:
       url: url of the container
       root: directory to store the content
    """
    # Get HTML request and parse the HTML for image links and title
-   
    reqs = requests.get(url)
    soup = BeautifulSoup(reqs.text, 'html.parser')
    while "500 Internal Server Error" in soup.find("title"):
@@ -146,10 +180,11 @@ def process_container(url:str, root:str)->None:
       reqs = requests.get(url)
       soup = BeautifulSoup(reqs.text, 'html.parser')
    imgLinks = soup.find_all("a", class_="fileThumb")
-   titleDir = root + (re.sub(r'[^\w\-_\. ]', '_', soup.find("title").contents[0]).strip()).split("/")[0] + "/"
+   titleDir = root + (re.sub(r'[^\w\-_\. ]', '_', soup.find("title").text.strip())).split("/")[0] + "/"
    if not os.path.isdir(titleDir):
-      os.mkdir(titleDir)   # 500 internal system error
+      os.makedirs(titleDir) 
 
+   reqs.close()
    # Download image links
    download_all_files(imgLinks, titleDir)
 
@@ -175,7 +210,7 @@ def process_container(url:str, root:str)->None:
             download_queue.put((containerPrefix + download, titleDir + trim_fname(download)))
             downloadables.release()
 
-def process_window(url:str)->None:
+def process_window(url:str, continuous:bool)->None:
    """
    Processes a single main artist window
    Param: 
@@ -183,7 +218,7 @@ def process_window(url:str)->None:
    """
    reqs = requests.get(url)
    soup = BeautifulSoup(reqs.text, 'html.parser')
-
+   reqs.close()
    # Create directory
    artist = soup.find("meta", attrs={'name': 'artist_name'})
    titleDir = folder + re.sub(r'[^\w\-_\. ]', '_', artist.get('content')) + "/"
@@ -201,36 +236,74 @@ def process_window(url:str)->None:
          content = link.find("a")
          process_container(containerPrefix + content.get('href'), titleDir)
       
-      # Move to next window
-      time.sleep(TIME_BETWEEN_CHUNKS)
-      counter += 25
-      reqs = requests.get(url + suffix + str(counter))
-      soup = BeautifulSoup(reqs.text, 'html.parser')
-      contLinks = soup.find_all("div", class_="post-card__link")
+      if continuous:
+         # Move to next window
+         time.sleep(TIME_BETWEEN_CHUNKS)
+         counter += 25
+         reqs = requests.get(url + suffix + str(counter))
+         soup = BeautifulSoup(reqs.text, 'html.parser')
+         reqs.close()
+         contLinks = soup.find_all("div", class_="post-card__link")
+      else:
+         contLinks = None
 
 
-def routine(url:str)->None:
+def call_and_interpret_url(url:str)->None:
    """
-   Main routine, processes a main artist window using multithreading
-   if url is None, ask for a url
+   Calls a function based on url type
+   https://kemono.party/fanbox/user/xxxx -> process_window()
+   https://kemono.party/fanbox/user/xxxx?o=xx -> process_window() one page only
+   https://kemono.party/fanbox/user/xxxx/post/xxxx -> process_container()
+
+   Anything else -> UnknownURLTypeException
+
    Param:
-      url: url of main artist window or None
+      url: url to process
+   Raise:
+      UnknownURLTypeException when url type cannot be determined
    """
-   # Get url to download
-   if url == None:
-      url = input("Input a url> ")
+   if '?' in url:
+      process_window(url, False)
+   elif "post" in url:
+      # Build directory
+      reqs = requests.get(url)
+      if(reqs.status_code  >= 400):
+         print("Status code " + str(reqs.status_code))
+      soup = BeautifulSoup(reqs.text, 'html.parser')
+      artist = soup.find("a", attrs={'class': 'post__user-name'})
+      titleDir = folder + re.sub(r'[^\w\-_\. ]', '_', artist.text.strip()) + "/"
+      if not os.path.isdir(titleDir):
+         os.makedirs(titleDir)
+      reqs.close()
 
+      # Process container
+      process_container(url, titleDir)
+   elif 'user' in url:
+      process_window(url, True)
+   else:
+      raise UnknownURLTypeException
+
+def create_threads(count:int)->list:
+   """
+   Creates count number of downThreads
+
+   Param:
+      count: how many threads to create
+   """
    threads = []
    # Spawn threads 
-   for i in range (0, THREADS):
+   for i in range (0, count):
       threads.append(downThread(i))
       threads[i].start()
+   return threads
 
-   process_window(url)
+def kill_threads(threads:list)->None:
+   """
+   Kills all threads in threads
 
-   # Wait until queue is empty
-   download_queue.join()
-
+   Param:
+      threads: threads to kill
+   """
    global kill
    kill = True
    
@@ -242,18 +315,65 @@ def routine(url:str)->None:
    
    kill = False
 
+def routine(url:str)->None:
+   """
+   Main routine, processes a main artist window using multithreading
+   if url is None, ask for a url.
+   Threads must be started and ended outside of routine and download queue
+   may still contain items
+   Param:
+      url: url of main artist window or None
+   """
+   # Get url to download
+   while url == None or "https://kemono.party" not in url:
+      url = input("Input a url, or type 'quit' to exit> ")
+
+      if(url == 'quit'):
+         return
+
+
+   # Interpret the url
+   call_and_interpret_url(url)
+
+
 def main()->None:
    """
    Program runner
    """
-   if len(sys.argv) > 1 and sys.argv[1] == "-f":
-      with open(sys.argv[2], "r") as fd:
-         url = fd.readline().strip()
-         while(len(url) > 0):
-            routine(url)
-            url = fd.readline().strip()
+   global folder
+   threads = None
+   if len(sys.argv) > 1:
+      pointer = 1
+      while(len(sys.argv) > pointer):
+         if sys.argv[pointer] == '-f' and len(sys.argv) >= pointer:
+            with open(sys.argv[pointer + 1], "r") as fd:
+               for line in fd:
+                  line = line.strip()
+                  if len(line) > 0:
+                     threads = create_threads(THREADS)
+                     routine(line)
+            pointer += 2
+         elif sys.argv[pointer] == '-d' and len(sys.argv) >= pointer:
+            folder = sys.argv[pointer + 1]
+            pointer += 2
+         else:
+            print("Switches")
+            print("No switch : Prompts user with download url")
+            print("-f <textfile.txt> : Download from text file containing links")
+            print("-d <path> : Set download path for single instance, must use '/'")
+            print("-d <path> -f <textfile.txt> : Combine -d and -f")
+            print("-h : Help")
+            exit()
+         
+         if not threads:
+            threads = create_threads(THREADS)
+            routine(None)
+      
+         
    else:
       routine(None)
 
+   download_queue.join()
+   kill_threads(threads)
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -10,9 +10,56 @@ Instructions:
 - Enter in a url of an artist home page (Follows this format: https://kemono.party/fanbox/user/xxxxxxxx)
 - Enjoy!
 
+Command line arguments:
+KMPDownloader.py -f <.txt>      Bulk downloads all links in .txt file
+KMPDownloader.py -d <path>      Sets download path for a single download instance, must use /
+KMPDownloader.py -d <path> -f <.txt>    Combines above 2 switches
+KMPDownloader.py -h             Help
+KMPDownloader.py                Prompts user to enter a url
+
+Bulk file example format / Supported URL formats:
+<File/>
+https://kemono.party/service/user/xxxxxx
+https://kemono.party/service/user/xxxxxx?o=25
+https://kemono.party/service/user/xxxxxx/post/xxx
+</File>
+
+https://kemono.party/service/user/xxxxxx -> Downloads all artist works
+https://kemono.party/service/user/xxxxxx?o=25 -> Downloads all artist works on that specific page
+https://kemono.party/service/user/xxxxxx/post/xxx -> Downloads specific artist work
+
+Download information:
+- There is currently no way to change download options without changing the code directly
+- All files will be downloaded in dynamically generated paths from a given download path
+
+Example
+1) User wants to download https://kemono.party/service/user/xxxxxx/post/xxx in path C:/user/contents/
+2) System derives data from link (artist name, post name, post content, downloads, and files)
+3) Downloads to C:/user/contents/artist/postname/
+- Post content saved to post_content.txt
+- downloads saved using default name
+- Files downloaded with a counter 0..n where 0 is the first file on the page and n is the last file on the page
+
 Known bugs:
-- No DDOS guard countermeasures, leading to 404 errors (solution is to use a VPN for now and decreasing download rate)
 - post_content.txt may contain garbage data at times
+
+Possible bugs:
+These bugs were accounted for but not enough testing has been conducted
+- DDOS preventing downloads (Does not throw an exception)
+- Large files timing out before downloaded fully (Does not throw an exception)
+
+Changelog 0.3:
+- Bulk download support of urls for all artist work, single page of artist work, or a single artist work
+- Multithreading optimization where all urls in bulk download file can be loaded up into a queue instead of having to wait for a url to finish downloading
+- Several robust upgrades
+- Slight memory optimization
+- Fixed bug where certain files names were not being trimmed properly
+- Switch to cfscrape library instead of requests for Kemono's ddos protection
+- Removed chunk download support and visable download process temporarily, download process can be monitored in task manager perforance tab in network section
+- Disposal of most invalid urls and option to quit ran with no input url
+- Removed duplicate file check temporarily
+- Added command line switches -f and -d
+- Fixed bug where downloading large files would timeout before they were completed.
 
 Changelog 0.2:
 - Multithreading support
@@ -31,6 +78,6 @@ Changelog 0.1:
 Future features:
 - User friendly upgrades
 - GUI
-- Command line argument support
-- Automatic Gdrive and Mega download from links
-- DDOS solution (headless selenium?)
+- Command line argument support for more settings
+- Automatic Gdrive and Mega download from links <unlikely>
+- 
